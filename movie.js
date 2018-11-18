5c6dc3cf1469873570789332c9e0b6c9ca63cf4d
Nilay Neeranjun <nneeranjun@Nilays-MacBook-Pro.local>
2018-11-18 12:00:05 -0800

    Everything except removing/adding 1 to quantity is done

---
+++ movie.js
@@ -0,0 +1,52 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: [
+            {id: 0, name: 'Star Wars Episode IV DVD ($20)', blueray: false, price: 20 },
+            {id: 1, name: 'Star Wars Episode V DVD ($20)', blueray: false, price: 20 },
+            {id: 2, name: 'Star Wars Episode VI DVD ($20)', blueray: false, price: 20 },
+            {id: 3, name: 'Star Wars Episode IV DVD ($25)', blueray: true, price: 25 },
+            {id: 4, name: 'Star Wars Episode V DVD ($25)', blueray: true, price: 25 },
+            {id: 5, name: 'Star Wars Episode VI DVD ($25)', blueray: true, price: 25 }
+        ],
+        cart: [
+            
+        ],
+        size: 0,
+        total: 0
+    },
+    methods: {
+        addToCart: function(index) {
+            var movie = app.items[index];
+            var item = app.cart.find(obj => {
+                return obj.item.id === movie.id
+            });
+            if (jQuery.isEmptyObject(item)) {
+                app.cart.push({item: movie, quantity: 1});
+            } else {
+                item.quantity += 1;
+                console.log(item.quantity);
+            } 
+            app.size += 1;
+            app.total += movie.price;
+        },
+        
+        removeFromCart: function(movie) {
+            var filtered = app.cart.filter(function(value){
+                return value.item.id !=  movie.item.id;
+            });
+            app.cart = filtered;
+            app.size -= movie.quantity;
+            app.total -= (movie.item.price * movie.quantity); 
+        }
+        
+    }
+});
+
+$(document).ready(function() {
+    $('#cart').popover({
+        content: $('#popover-content')
+    }).on('show.bs.popover', function() {
+        $('#popover-content').removeClass('d-none')
+    });
+})
